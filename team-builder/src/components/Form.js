2295895ed839598d885eb1068292c447c5d92b28
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2019-10-07 21:42:56 -0400

    Added Form.js

---
+++ team-builder/src/components/Form.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function Form(props) {
+  return (
+    <form>
+      <label>
+        Name:
+        <input />
+      </label>
+
+      <label>
+        Email:
+        <input />
+      </label>
+
+      <label>
+        Role: 
+        <input />
+      </label>
+    </form>
+  );
+}
