eccb709a54393441b7b59c3b94fcf2fb80fac602
tiffany-simionescu <tiffanyfeldkamp@gmail.com>
2019-10-10 20:58:49 -0400

    formatted code

--- team-builder/src/components/TeamMembers.js
+++ team-builder/src/components/TeamMembers.js
@@ -6,9 +6,14 @@ export default function TeamMembers(props) {
       {props.memberList.map(member => {
         return (
           <div className="member" key={member.id}>
-            <p><span className="title">Name:</span> <span className="name-outcome">{member.name}</span></p>
-            <p><span className="title">Email:</span> <span className="email-outcome">{member.email}</span></p>
-            <p><span className="title">Role:</span> <span className="role-outcome">{member.role}</span></p>
+            <p><span className="title">Name:</span> 
+              <span className="name-outcome">{member.name}</span></p>
+
+            <p><span className="title">Email:</span> 
+              <span className="email-outcome">{member.email}</span></p>
+              
+            <p><span className="title">Role:</span>
+              <span className="role-outcome">{member.role}</span></p>
             <hr />
           </div>
         );
